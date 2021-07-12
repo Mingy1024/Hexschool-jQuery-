f3e58e6f9f0ec09e7b0712c17a09de47bc57b7f2
Mingy1024 <s2142717@gmail.com>
2021-07-12 15:28:03 +0800

    新增lightbox

--- all.js
+++ all.js
@@ -1,40 +1,48 @@
 $(document).ready(function () {
-    // swiper
-    const swiper = new Swiper(".swiper-container", {
-        // Optional parameters
-        direction: "horizontal",
-        speed:1000,
-        loop: true,
-        autoplay: {
-          delay:2000
-        },
-      
-        // If we need pagination
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      
-      });
 
-    //下拉式選單
-    $(".menu-link1").click(function (e) { 
-        e.preventDefault();
-        $(".drop1").slideToggle();
-        $(this).parent().siblings().find(".dropdown-list").slideUp();
-        $(".menu-link1").toggleClass("active");
-        $(this).parent().siblings().find("a").removeClass("active");
-    });
-    $(".menu-link2").click(function (e) { 
-        e.preventDefault();
-        $(".drop2").slideToggle();
-        $(this).parent().siblings().find(".dropdown-list").slideUp();
-        $(".menu-link2").toggleClass("active");
-        $(this).parent().siblings().find("a").removeClass("active");
-    });
-    //top效果
-    $(".back a").click(function (e) { 
-        e.preventDefault();
-        $("html,body").animate({ scrollTop:0},800);
-    });
+  //lightbox
+  lightbox.option({
+    resizeDuration: 500,
+    wrapAround: true,
+    positionFromTop:350,
+  });
+
+  // swiper
+  const swiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    direction: "horizontal",
+    speed: 1000,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  //下拉式選單
+  $(".menu-link1").click(function (e) {
+    e.preventDefault();
+    $(".drop1").slideToggle();
+    $(this).parent().siblings().find(".dropdown-list").slideUp();
+    $(".menu-link1").toggleClass("active");
+    $(this).parent().siblings().find("a").removeClass("active");
+  });
+  $(".menu-link2").click(function (e) {
+    e.preventDefault();
+    $(".drop2").slideToggle();
+    $(this).parent().siblings().find(".dropdown-list").slideUp();
+    $(".menu-link2").toggleClass("active");
+    $(this).parent().siblings().find("a").removeClass("active");
+  });
+
+  //top效果
+  $(".back a").click(function (e) {
+    e.preventDefault();
+    $("html,body").animate({ scrollTop: 0 }, 800);
+  });
 });
